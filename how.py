3550b5ba69ce19e9eb0945e0cbeb1147ec4468b7
Marc Laugharn <mlaugharn@gmail.com>
2023-03-01 21:26:46 -0800

    Create how.py

---
+++ how.py
@@ -0,0 +1,31 @@
+import openai
+import os
+
+# Define ANSI escape codes for console styling
+ANSI_BOLD = "\033[1m"
+ANSI_GREEN = "\033[32m"
+ANSI_RESET = "\033[0m"
+
+openai.api_key = os.getenv("OPENAI_API_KEY")
+
+def generate_answer(question):
+    completion = openai.ChatCompletion.create(
+        model="chatgpt",
+        prompt=f"{ANSI_BOLD}How do I {question}? (Type {ANSI_GREEN}'tldr'{ANSI_RESET} for a short summary){ANSI_RESET}\n",
+        temperature=0.7,
+        max_tokens=100,
+        n=1,
+        stop=None,
+        frequency_penalty=0,
+        presence_penalty=0,
+    )
+    answer = completion.choices[0].text.strip()
+    return answer
+
+if __name__ == '__main__':
+    question = input(f"{ANSI_BOLD}What do you want to know? {ANSI_RESET}")
+    if question.lower() == 'tldr':
+        print(f"{ANSI_BOLD}To get a short summary, type {ANSI_GREEN}'tldr'{ANSI_RESET} after the question prompt.{ANSI_RESET}")
+    else:
+        answer = generate_answer(question)
+        print(f"\n{ANSI_BOLD}Answer: {ANSI_RESET}{ANSI_GREEN}{answer}{ANSI_RESET}")
