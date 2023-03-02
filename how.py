c3208758efcf9e96040b3377f6cf5432f24c6dc7
Marc Laugharn <mlaugharn@gmail.com>
2023-03-01 21:38:58 -0800

    Update how.py

--- how.py
+++ how.py
@@ -1,5 +1,6 @@
 import openai
 import os
+import click
 
 # Define ANSI escape codes for console styling
 ANSI_BOLD = "\033[1m"
@@ -8,9 +9,11 @@ ANSI_RESET = "\033[0m"
 
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
+@click.command()
+@click.option('--question', '-q', prompt=f"{ANSI_BOLD}What computer-related question do you have?{ANSI_RESET}")
 def generate_answer(question):
     completion = openai.Completion.create(
-        engine="davinci-codex",
+        engine="text-davinci-002",
         prompt=f"{ANSI_BOLD}How do I {question}? (Type {ANSI_GREEN}'tldr'{ANSI_RESET} for a short summary){ANSI_RESET}\n",
         max_tokens=100,
         n=1,
@@ -25,9 +28,8 @@ def generate_answer(question):
     return answer
 
 if __name__ == '__main__':
-    question = input(f"{ANSI_BOLD}What computer-related question do you have? {ANSI_RESET}")
-    if question.lower() == 'tldr':
+    answer = generate_answer()
+    if answer.lower() == 'tldr':
         print(f"{ANSI_BOLD}To get a short summary, type {ANSI_GREEN}'tldr'{ANSI_RESET} after the question prompt.{ANSI_RESET}")
     else:
-        answer = generate_answer(question)
         print(f"\n{ANSI_BOLD}Answer: {ANSI_RESET}{ANSI_GREEN}{answer}{ANSI_RESET}")
