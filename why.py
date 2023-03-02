2a872b0a760038f961dafa8b562c8284bae1d98e
Marc Laugharn <mlaugharn@gmail.com>
2023-03-01 21:40:44 -0800

    Update why.py

--- why.py
+++ why.py
@@ -1,33 +1,41 @@
 import openai
 import os
+import click
 
 # Define ANSI escape codes for console styling
 ANSI_BOLD = "\033[1m"
-ANSI_CYAN = "\033[36m"
+ANSI_GREEN = "\033[32m"
 ANSI_RESET = "\033[0m"
 
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
-def explain_error(error_message):
+@click.command()
+@click.option('--tldr', '-t', is_flag=True, help='Provide a short summary')
+@click.argument('question', type=str)
+def generate_answer(tldr, question):
+    if tldr:
+        max_tokens = 50
+    else:
+        max_tokens = 100
+
+    prompt = f"{ANSI_BOLD}Why is {question}?{ANSI_RESET}\n"
+    if tldr:
+        prompt += f"{ANSI_BOLD}TLDR:{ANSI_RESET} "
+    prompt += "\n"
+
     completion = openai.Completion.create(
-        engine="davinci-codex",
-        prompt=f"{ANSI_BOLD}What does the error message '{error_message}' mean? (Type {ANSI_CYAN}'tldr'{ANSI_RESET} for a short summary){ANSI_RESET}\n",
-        max_tokens=100,
+        engine="text-davinci-002",
+        prompt=prompt,
+        max_tokens=max_tokens,
         n=1,
         stop=None,
         temperature=0.7,
         frequency_penalty=0,
         presence_penalty=0,
-        model="text-davinci-002",
         prompt_context=["computer-related"]
     )
-    explanation = completion.choices[0].text.strip()
-    return explanation
+    answer = completion.choices[0].text.strip()
+    return answer
 
 if __name__ == '__main__':
-    error_message = input(f"{ANSI_BOLD}What computer-related error did you get? {ANSI_RESET}")
-    if error_message.lower() == 'tldr':
-        print(f"{ANSI_BOLD}To get a short summary, type {ANSI_CYAN}'tldr'{ANSI_RESET} after the error message prompt.{ANSI_RESET}")
-    else:
-        explanation = explain_error(error_message)
-        print(f"\n{ANSI_BOLD}Explanation: {ANSI_RESET}{ANSI_CYAN}{explanation}{ANSI_RESET}")
+    generate_answer()
